64f7c2be62dfb655167c11657efbae6f9370724a
John Vogelsberg <jvogelsb@gmail.com>
2025-12-02 23:20:22 -0700

    cargo fmt

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -19,22 +19,22 @@ fn read_input(file_path: &str) -> String {
 
 fn parse_input(input: &str) -> Vec<i64> {
     let mut invalid_ids: Vec<i64> = vec![];
-     
+
     for rng in input.split(',') {
-        let parts:Vec<&str> = rng.split('-').collect();
+        let parts: Vec<&str> = rng.split('-').collect();
         println!("Range 0: {}\t1: {}", parts[0], parts[1]);
         let start = parts[0].parse::<i64>().expect("invalid start number");
         let end = parts[1].parse::<i64>().expect("invalid end number");
         println!("Range: {} - {}", start, end);
         for curr in start..=end {
             let string_curr: String = curr.to_string();
-            let str_curr:&str = &string_curr.as_str();
+            let str_curr: &str = &string_curr.as_str();
             let l = str_curr.len();
             if l % 2 != 0 {
                 continue;
-            }      
-            let front_val = &str_curr[0..l/2];
-            let end_val = &str_curr[l/2..];
+            }
+            let front_val = &str_curr[0..l / 2];
+            let end_val = &str_curr[l / 2..];
             if front_val == end_val {
                 invalid_ids.push(curr);
                 print!("\t{}", curr)
@@ -53,7 +53,7 @@ fn sum_ids(ids: Vec<i64>) -> i64 {
     sum
 }
 
-fn part_one(input: &str ) -> i64 {
+fn part_one(input: &str) -> i64 {
     let invalid_ids = parse_input(input);
     let sum_one = sum_ids(invalid_ids);
     sum_one
